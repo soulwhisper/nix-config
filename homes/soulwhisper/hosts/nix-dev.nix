5a416f2dc59df7937fb9d90569b590593da4a74f
Hekatos Noir <soulwhisper@outlook.com>
2024-10-12 12:23:40 +0800

    Update nix-dev.nix

--- homes/soulwhisper/hosts/nix-dev.nix
+++ homes/soulwhisper/hosts/nix-dev.nix
@@ -1,3 +1,23 @@
-_:
 {
+  lib,
+  pkgs,
+  ...
+}:
+{
+  modules = {
+    development.enable = true;
+    kubernetes.enable = true;
+    security.gnugpg.enable = true;
+    shell = {
+      mise = {
+        enable = true;
+        package = pkgs.unstable.mise;
+        globalConfig = {
+          tools = {
+            python = "latest";
+          };
+        };
+      };
+    };
+  };
 }
