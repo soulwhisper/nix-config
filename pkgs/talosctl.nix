beeeca1f2e6695b0901ac26d57bd464afb62256e
Hekatos Noir <soulwhisper@outlook.com>
2024-11-15 09:39:13 +0000

    feat(deps): auto-update vendorHash

--- pkgs/talosctl.nix
+++ pkgs/talosctl.nix
@@ -7,26 +7,33 @@
 let
   sourceData = pkgs.callPackage ./_sources/generated.nix { };
   packageData = sourceData.talosctl;
+  vendorHash = lib.importJSON ./_sources/vendorhash.json;
 in
 pkgs.unstable.buildGo123Module {
   inherit (packageData) pname src;
   version = lib.strings.removePrefix "v" packageData.version;
-  vendorHash = "sha256-pWG8DbZ9N57p2Q9w/IzETcvwaSfzaUvJgcz7Th/Oi9c=";
+  vendorHash = vendorHash.talosctl;
+
   ldflags = ["-s" "-w"];
+
   # This is needed to deal with workspace issues during the build
   overrideModAttrs = _: {
     GOWORK = "off";
   };
-  GOWORK = "off";
+
   subPackages = ["cmd/talosctl"];
+
   nativeBuildInputs = [installShellFiles];
+
   postInstall = ''
     installShellCompletion --cmd talosctl \
       --bash <($out/bin/talosctl completion bash) \
       --fish <($out/bin/talosctl completion fish) \
       --zsh <($out/bin/talosctl completion zsh)
   '';
+
   doCheck = false; # no tests
+
   meta = with lib; {
     description = "A CLI for out-of-band management of Kubernetes nodes created by Talos";
     homepage = "https://www.talos.dev/";
