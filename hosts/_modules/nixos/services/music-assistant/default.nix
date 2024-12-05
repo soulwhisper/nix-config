bf4ec742d98f0ac58f7565df38d8f86f46fa8b54
Hekatos Noir <soulwhisper@outlook.com>
2024-12-05 09:09:17 +0000

    feat(nix-nas): improve reverse proxy QoL - remap web ports - migrate from nginx to caddy

--- hosts/_modules/nixos/services/music-assistant/default.nix
+++ hosts/_modules/nixos/services/music-assistant/default.nix
@@ -10,27 +10,23 @@ in
 {
   options.modules.services.music-assistant = {
     enable = lib.mkEnableOption "music-assistant";
-    configDir = lib.mkOption {
-      type = lib.types.path;
-      default = "/var/lib/music-assistant";
-    };
   };
 
   config = lib.mkIf cfg.enable {
     services.music-assistant = {
       enable = true;
-	package = pkgs.unstable.music-assistant;
-	providers = [
-	  "filesystem_local"
-	  "filesystem_smb"
-	  "hass"
-	  "hass_players"
-       ];
-       extraOptions = [
-	  "--config"
-	  "${cfg.configDir}"
-	  "--log-level"
-	  "DEBUG"
+	    package = pkgs.unstable.music-assistant;
+	    providers = [
+	      "filesystem_local"
+	      "filesystem_smb"
+	      "hass"
+	      "hass_players"
+      ];
+      extraOptions = [
+	      "--config"
+	      "/var/lib/music-assistant"
+	      "--log-level"
+	      "DEBUG"
       ];
     };
   };
