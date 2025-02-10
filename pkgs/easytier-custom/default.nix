ce33b8a995938ef99ba05672adda1a55fceb20ef
Hekatos Noir <soulwhisper@outlook.com>
2025-02-10 00:43:26 +0000

    fix

--- pkgs/easytier-custom/default.nix
+++ pkgs/easytier-custom/default.nix
@@ -12,7 +12,7 @@ in
     inherit (packageData) pname src;
     version = lib.strings.removePrefix "v" packageData.version;
     cargoHash = vendorHash.easytier-custom;
-    useFetchCargoVendor = true
+    useFetchCargoVendor = true;
 
     nativeBuildInputs = [pkgs.protobuf];
 
