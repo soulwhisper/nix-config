fe8579cc381245d65654d4428b6ee2a7fe438d5e
soulwhisper <26273512+soulwhisper@users.noreply.github.com>
2025-02-10 00:20:15 +0000

    chore(lint): fix linting issues

--- pkgs/easytier-custom/default.nix
+++ pkgs/easytier-custom/default.nix
@@ -6,31 +6,30 @@
   nix-update-script,
   darwin,
   ...
-}:
-let
-  sourceData = pkgs.callPackage ../_sources/generated.nix { };
+}: let
+  sourceData = pkgs.callPackage ../_sources/generated.nix {};
   vendorHash = lib.importJSON ../vendorhash.json;
   packageData = sourceData.easytier-custom;
 in
-rustPlatform.buildRustPackage rec {
-  inherit (packageData) pname src;
-  version = lib.strings.removePrefix "v" packageData.version;
-  cargoHash = vendorHash.easytier-custom;
+  rustPlatform.buildRustPackage rec {
+    inherit (packageData) pname src;
+    version = lib.strings.removePrefix "v" packageData.version;
+    cargoHash = vendorHash.easytier-custom;
 
-  nativeBuildInputs = [ protobuf ];
+    nativeBuildInputs = [protobuf];
 
-  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
-    darwin.apple_sdk.frameworks.Security
-  ];
+    buildInputs = lib.optionals stdenv.hostPlatform.isDarwin [
+      darwin.apple_sdk.frameworks.Security
+    ];
 
-  doCheck = false; # no tests
+    doCheck = false; # no tests
 
-  passthru.updateScript = nix-update-script { };
+    passthru.updateScript = nix-update-script {};
 
-  meta = {
-    homepage = "https://github.com/EasyTier/EasyTier";
-    description = "Simple, decentralized mesh VPN with WireGuard support";
-    changelog = "https://github.com/EasyTier/EasyTier/releases/tag/v${version}";
-    mainProgram = "easytier-core";
-  };
-}
+    meta = {
+      homepage = "https://github.com/EasyTier/EasyTier";
+      description = "Simple, decentralized mesh VPN with WireGuard support";
+      changelog = "https://github.com/EasyTier/EasyTier/releases/tag/v${version}";
+      mainProgram = "easytier-core";
+    };
+  }
