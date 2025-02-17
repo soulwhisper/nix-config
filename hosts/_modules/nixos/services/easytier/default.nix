506ee7ad755545334636c37e978fff2c785f2bbd
Hekatos Noir <soulwhisper@outlook.com>
2025-02-17 04:47:44 +0000

    fix

--- hosts/_modules/nixos/services/easytier/default.nix
+++ hosts/_modules/nixos/services/easytier/default.nix
@@ -5,35 +5,41 @@
   ...
 }: let
   cfg = config.modules.services.easytier;
-  py-toml-merge = pkgs.writers.writePython3Bin "py-toml-merge" {
-    libraries = with pkgs.python3Packages; [
-      tomli-w
-      mergedeep
-    ];
-  }
-  ''
-    import argparse
-    from pathlib import Path
-    from typing import Any
-    import tomli_w
-    import tomllib
-    from mergedeep import merge
+  py-toml-merge =
+    pkgs.writers.writePython3Bin "py-toml-merge"
+      {
+        libraries = with pkgs.python3Packages; [
+          tomli-w
+          mergedeep
+        ];
+      }
+      ''
+        import argparse
+        from pathlib import Path
+        from typing import Any
 
-    parser = argparse.ArgumentParser(description="Merge multiple TOML files")
-    parser.add_argument(
-      "files",
-      type=Path,
-      nargs="+",
-      help="List of TOML files to merge",
-    )
-    args = parser.parse_args()
-    merged: dict[str, Any] = {}
-    for file in args.files:
-      with open(file, "rb") as fh:
-        loaded_toml = tomllib.load(fh)
-        merged = merge(merged, loaded_toml)
-    print(tomli_w.dumps(merged))
-  '';
+        import tomli_w
+        import tomllib
+        from mergedeep import merge
+
+        parser = argparse.ArgumentParser(description="Merge multiple TOML files")
+        parser.add_argument(
+            "files",
+            type=Path,
+            nargs="+",
+            help="List of TOML files to merge",
+        )
+
+        args = parser.parse_args()
+        merged: dict[str, Any] = {}
+
+        for file in args.files:
+            with open(file, "rb") as fh:
+                loaded_toml = tomllib.load(fh)
+                merged = merge(merged, loaded_toml)
+
+        print(tomli_w.dumps(merged))
+      '';
   toml = pkgs.formats.toml {};
   mkConfig = toml.generate "config.toml" {
     instance_name = "default";
