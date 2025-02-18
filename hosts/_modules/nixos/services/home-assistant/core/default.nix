24d7c76bf65206060bdeb9f0316150cb48bbe4af
Hekatos Noir <soulwhisper@outlook.com>
2025-02-18 05:34:54 +0000

    fix(hass): lovelace use storage, hass use unstable

--- hosts/_modules/nixos/services/home-assistant/core/default.nix
+++ hosts/_modules/nixos/services/home-assistant/core/default.nix
@@ -19,28 +19,32 @@ in {
     services.home-assistant = {
       enable = true;
       configDir = "${cfg.dataDir}/core";
-
-      extraComponents = [
-        "default_config"
-        "ffmpeg"
-        "homekit"
-        "homekit_controller"
-        "met"
-      ];
+      package = pkgs.unstable.home-assistant.override {
+        extraComponents = [
+          "default_config"
+          "ffmpeg"
+          "homekit"
+          "homekit_controller"
+          "met"
+        ];
+      };
       extraPackages = python3Packages:
         with python3Packages; [
+          aiohomekit
           gtts
           isal
           pyatv
+          python-otbr-api
+          radios
           zlib-ng
         ];
-      customComponents = with pkgs.home-assistant-custom-components; [
+      customComponents = with pkgs.unstable.home-assistant-custom-components; [
         midea_ac_lan
         ntfy
         prometheus_sensor
         xiaomi_miot
       ];
-      customLovelaceModules = with pkgs.home-assistant-custom-lovelace-modules; [
+      customLovelaceModules = with pkgs.unstable.home-assistant-custom-lovelace-modules; [
         atomic-calendar-revive
         bubble-card
         button-card
@@ -182,216 +186,6 @@ in {
                 device_class = "monetary";
               }
             ];
-          }
-        ];
-      };
-
-      lovelaceConfigWritable = true;
-      lovelaceConfig = {
-        views = [
-          {
-            title = "Home";
-            path = "home";
-            type = "sections";
-            sections = [
-              {
-                type = "grid";
-                cards = [
-                  {
-                    show_current = true;
-                    show_forecast = true;
-                    type = "weather-forecast";
-                    entity = "weather.forecast_wo_de_jia";
-                    forecast_type = "daily";
-                  }
-                  {
-                    type = "markdown";
-                    content = "- 标题开关仅控制开关，不含窗帘";
-                  }
-                  {
-                    type = "grid";
-                    square = false;
-                    columns = 1;
-                    cards = [
-                      {
-                        type = "entities";
-                        entities = [
-                          {entity = "cover.curtain";}
-                          {entity = "switch.wall_switch_switch1_2";}
-                          {entity = "switch.wall_switch_switch2";}
-                          {entity = "switch.wall_switch_switch3";}
-                          {entity = "switch.wall_switch_switch2_2";}
-                          {entity = "switch.wall_switch_2";}
-                          {entity = "switch.wall_switch";}
-                        ];
-                        title = "客厅";
-                        show_header_toggle = true;
-                      }
-                    ];
-                  }
-                  {
-                    type = "entities";
-                    entities = [
-                      "switch.wall_switch_switch1_3"
-                      "switch.wall_switch_switch2_3"
-                    ];
-                    title = "厨房";
-                    show_header_toggle = true;
-                  }
-                ];
-              }
-              {
-                type = "grid";
-                cards = [
-                  {
-                    type = "custom:mini-graph-card";
-                    entities = [
-                      {
-                        entity = "sensor.last_electricity_usage";
-                        name = "国网七天用电曲线";
-                        aggregate_func = "first";
-                        show_state = true;
-                        show_points = true;
-                      }
-                    ];
-                    group_by = "date";
-                    hour24 = true;
-                    hours_to_show = 240;
-                  }
-                  {
-                    type = "custom:mushroom-entity-card";
-                    entity = "sensor.month_electricity_charge";
-                    icon = "mdi:home";
-                    name = "上月国网电费";
-                    tap_action = {action = "none";};
-                    hold_action = {action = "none";};
-                    double_tap_action = {action = "none";};
-                    icon_type = "icon";
-                    fill_container = false;
-                    layout_options = {
-                      grid_columns = 4;
-                      grid_rows = 1;
-                    };
-                  }
-                  {
-                    type = "custom:mushroom-entity-card";
-                    entity = "sensor.yearly_electricity_charge";
-                    name = "今年国网总电费";
-                    icon = "mdi:currency-cny";
-                    layout_options = {
-                      grid_columns = 2;
-                      grid_rows = 1;
-                    };
-                    tap_action = {action = "none";};
-                    hold_action = {action = "none";};
-                    double_tap_action = {action = "none";};
-                    fill_container = true;
-                  }
-                  {
-                    type = "custom:mushroom-entity-card";
-                    entity = "sensor.yearly_electricity_usage";
-                    name = "今年国网总电量";
-                    icon = "mdi:lightning-bolt";
-                    tap_action = {action = "none";};
-                    hold_action = {action = "none";};
-                    double_tap_action = {action = "none";};
-                  }
-                  {
-                    type = "tile";
-                    entity = "climate.211106241676463_climate";
-                    layout_options = {
-                      grid_columns = 4;
-                      grid_rows = 1;
-                    };
-                    features = [
-                      {type = "climate-hvac-modes";}
-                    ];
-                  }
-                  {
-                    type = "entities";
-                    entities = [
-                      "switch.wall_switch_switch2_4"
-                      "switch.wall_switch_switch3_2"
-                      "switch.wall_switch_switch1_4"
-                    ];
-                    title = "书房";
-                    show_header_toggle = true;
-                  }
-                  {
-                    type = "entities";
-                    entities = [
-                      "cover.curtain_2"
-                      "switch.wall_switch_switch1_5"
-                      "switch.wall_switch_switch2_5"
-                    ];
-                    title = "卧室";
-                    show_header_toggle = true;
-                  }
-                ];
-              }
-            ];
-            max_columns = 2;
-            cards = [];
-          }
-          {
-            path = "default_view";
-            title = "Default";
-            cards = [
-              {
-                type = "grid";
-                square = false;
-                columns = 1;
-                cards = [
-                  {
-                    type = "entities";
-                    entities = [
-                      {entity = "cover.curtain";}
-                      {entity = "switch.wall_switch_switch1_2";}
-                      {entity = "switch.wall_switch_switch2";}
-                      {entity = "switch.wall_switch_switch3";}
-                      {entity = "switch.wall_switch_switch2_2";}
-                      {entity = "switch.wall_switch_2";}
-                      {entity = "switch.wall_switch";}
-                    ];
-                    title = "客厅";
-                  }
-                ];
-              }
-              {
-                type = "entities";
-                entities = [
-                  "switch.wall_switch_switch1_3"
-                  "switch.wall_switch_switch2_3"
-                ];
-                title = "厨房";
-              }
-              {
-                type = "entities";
-                entities = [
-                  "cover.curtain_2"
-                  "switch.wall_switch_switch1_5"
-                  "switch.wall_switch_switch2_5"
-                ];
-                title = "卧室";
-              }
-              {
-                type = "entities";
-                entities = [
-                  "switch.wall_switch_switch2_4"
-                  "switch.wall_switch_switch3_2"
-                  "switch.wall_switch_switch1_4"
-                ];
-                title = "书房";
-              }
-              {
-                show_current = true;
-                show_forecast = true;
-                type = "weather-forecast";
-                entity = "weather.forecast_wo_de_jia";
-                forecast_type = "daily";
-              }
-            ];
-            visible = [];
           }
         ];
       };
