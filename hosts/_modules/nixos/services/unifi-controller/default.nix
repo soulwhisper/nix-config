6b60fb02f961b8676a6a2cd1b8bc3790f4cd9edc
Hekatos Noir <soulwhisper@outlook.com>
2025-03-25 18:36:20 +0800

    patch(unifi): fix ram usage issue

--- hosts/_modules/nixos/services/unifi-controller/default.nix
+++ hosts/_modules/nixos/services/unifi-controller/default.nix
@@ -25,9 +25,21 @@ in {
       "L /var/lib/unifi - - - - ${cfg.dataDir}"
     ];
 
+    # use same parameters in "lscr.io/linuxserver/unifi-network-application:latest"
     services.unifi = {
       enable = true;
       mongodbPackage = pkgs.mongodb-ce;
+      initialJavaHeapSize = 1024;
+      maximumJavaHeapSize = 1024;
+      extraJvmOptions = [
+        "-Dlog4j2.formatMsgNoLookups=true"
+        "-Dfile.encoding=UTF-8"
+        "-Djava.awt.headless=true"
+        "-Dapple.awt.UIElement=true"
+        "-XX:+UseParallelGC"
+        "-XX:+ExitOnOutOfMemoryError"
+        "-XX:+CrashOnOutOfMemoryError"
+      ];
     };
   };
 }
