927bbdb713b868993c708b23b8190564d4dd28a5
soulwhisper <26273512+soulwhisper@users.noreply.github.com>
2025-04-02 06:47:12 +0000

    chore(deps): Update packages managed by nvfetcher

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -69,4 +69,15 @@
       sha256 = "sha256-2YKZfW62yOA8uV3bn6at/9YV3OHjiMuqA1SUupyAAx4=";
     };
   };
+  zotregistry = {
+    pname = "zotregistry";
+    version = "v2.1.2";
+    src = fetchFromGitHub {
+      owner = "project-zot";
+      repo = "zot";
+      rev = "v2.1.2";
+      fetchSubmodules = false;
+      sha256 = "sha256-tVvqNA7imLmVQoO7giEtktJkgQTaaWjkB/aYM+or6kg=";
+    };
+  };
 }
