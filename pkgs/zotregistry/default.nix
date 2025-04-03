6e6cf28d806007417381db03066d6fd581154ee0
Hekatos Noir <soulwhisper@outlook.com>
2025-04-03 07:34:42 +0000

    patch

--- pkgs/zotregistry/default.nix
+++ pkgs/zotregistry/default.nix
@@ -12,6 +12,8 @@
     inherit (sourceData.zotregistry-ui) pname src version;
     npmDepsHash = "sha256-5f9D+DmX4I14wx5mNScero1xWQRtuLwhfDXfHM0mbB4=";
 
+    npmFlags = [ "--legacy-peer-deps" ];
+
     installPhase = ''
       runHook preInstall
       cp -r . "$out"
