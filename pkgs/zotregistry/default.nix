5e759c311c73474a07b76e4676ea7ef09b48207d
Hekatos Noir <soulwhisper@outlook.com>
2025-04-03 13:15:16 +0800

    Update default.nix

--- pkgs/zotregistry/default.nix
+++ pkgs/zotregistry/default.nix
@@ -1,66 +1,64 @@
 {
   pkgs,
   lib,
+  installShellFiles,
   ...
 }: let
   sourceData = pkgs.callPackage ../_sources/generated.nix {};
   packageData = sourceData.zotregistry;
   vendorData = lib.importJSON ../vendorhash.json;
+
+  uiData = sourceData.zotregistry-ui;
 in
   pkgs.unstable.buildGoModule rec {
     inherit (packageData) pname src;
     version = lib.strings.removePrefix "v" packageData.version;
     vendorHash = vendorData.zotregistry;
-
-    # fix: module lookup disabled by GOPROXY=off
+    
     # proxyVendor = true;
 
-    configurePhase = ''
-      export GOCACHE="$TMPDIR/go-cache"
-      export GOPATH="$TMPDIR/go"
-      export GOPROXY="https://goproxy.cn,direct"
-    '';
+    nativeBuildInputs = [installShellFiles];
 
-    postPatch = ''
-      substituteInPlace go.mod \
-        --replace-fail "cel.dev/expr" "github.com/google/cel-spec"
-      substituteInPlace go.mod \
-        --replace-fail "cloud.google.com/go" "github.com/googleapis/google-cloud-go"
-      substituteInPlace go.mod \
-        --replace-fail "cuelabs.dev/go/oci/ociregistry" "github.com/cue-labs/oci"
-      substituteInPlace go.mod \
-        --replace-fail "cuelang.org/go" "github.com/cue-lang/cue"
-      substituteInPlace go.mod \
-        --replace-fail "dario.cat/mergo" "github.com/imdario/mergo"
-      substituteInPlace go.mod \
-        --replace-fail "filippo.io/edwards25519" "github.com/FiloSottile/edwards25519"
-      substituteInPlace Makefile \
-        --replace-fail "$(shell which stacker)" ""
-    '';
-
-    nativeBuildInputs = with pkgs; [
-      git
-      nodejs
+    subPackages = [
+      "cmd/zli"
+      "cmd/zot"
     ];
 
-    # default = linux-amd64
-    buildPhase = ''
-      make OS=linux ARCH=amd64 binary cli
+    preBuild = ''
+      tar xvzf ${uiData.src} -C pkg/extensions/
     '';
+
+    tags = [
+      "imagetrust,lint,metrics,mgmt,profile,scrub,search,sync,ui,userprefs,containers_image_openpgp"
+    ];
+
+    ldflags = [
+      "-X zotregistry.dev/zot/pkg/api/config.Commit=v${version}"
+		  "-X zotregistry.dev/zot/pkg/api/config.BinaryType=-imagetrust-lint-metrics-mgmt-profile-scrub-search-sync-ui-userprefs"
+		  "-X zotregistry.dev/zot/pkg/api/config.GoVersion=${lib.getVersion go}"
+      "-s"
+      "-w"
+    ];
 
     doCheck = false;
 
-    ldflags = ["-s" "-w"];
-
     installPhase = ''
       runHook preInstall
-      install -Dm755 bin/zot-linux-amd64 -t $out/bin/zotregistry
-      install -Dm755 bin/zli-linux-amd64 -t $out/bin/zotregistry-cli
+      install -Dm755 bin/zli-* -t $out/bin/zli
+      install -Dm755 bin/zot-* -t $out/bin/zot
       runHook postInstall
     '';
 
+    postInstall = ''
+      installShellCompletion --cmd zli \
+        --bash <($out/bin/zli completion bash) \
+        --fish <($out/bin/zli completion fish) \
+        --zsh <($out/bin/zli completion zsh)
+    '';
+
+    # defaultPlatform = linux-amd64
     meta = {
-      mainProgram = "zotregistry";
+      mainProgram = "zot";
       description = "A scale-out production-ready vendor-neutral OCI-native container image/artifact registry.";
       homepage = "https://github.com/project-zot/zot";
       changelog = "https://github.com/project-zot/zot/releases/tag/v${version}";
