3718f15b0d21e29cf23d37e86fc6509544b7f67b
Hekatos Noir <soulwhisper@outlook.com>
2025-05-24 23:13:22 +0800

    fix(ci): fix nix-darwin sudo

--- hosts/soulwhisper-mba/default.nix
+++ hosts/soulwhisper-mba/default.nix
@@ -19,6 +19,8 @@
       openssh.authorizedKeys.keys = lib.strings.splitString "\n" (builtins.readFile ../../homes/soulwhisper/config/ssh/ssh.pub);
     };
 
+    system.primaryUser = "soulwhisper"; # since 25.05, nix-darwin needs this to be functional
+
     system.activationScripts.postActivation.text = ''
       # Must match what is in /etc/shells
       sudo chsh -s /run/current-system/sw/bin/fish soulwhisper
