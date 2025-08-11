b23f36c84dd4923579711f2f0cf95c943ffbb898
Hekatos Noir <soulwhisper@outlook.com>
2025-08-11 15:57:40 +0800

    patch(ssh): add ssh pwd hack

--- homes/_modules/security/ssh/default.nix
+++ homes/_modules/security/ssh/default.nix
@@ -1,8 +1,11 @@
 {
   config,
   lib,
+  pkgs,
   ...
-}: {
+}: let
+  inherit (config.home) username homeDirectory;
+in {
   config = {
     programs.ssh = {
       enable = true;
@@ -11,6 +14,10 @@
       includes = [
         "config.d/*"
       ];
+      extraConfig = ''
+        PermitLocalCommand yes
+        LocalCommand ssh_pwd_update %h
+      '';
 
       matchBlocks = {
         "192.168.*.*" = {
@@ -35,6 +42,40 @@
           };
         };
       };
+    };
+
+    programs.fish = {
+      functions = {
+        ssh_pwd_update = {
+          description = "ssh host dir hack";
+          body = ''
+            if test (count $argv) -ne 1
+              echo "Error: For usage from ssh_config only" >&2
+              return 1
+            end
+            set host $argv[1]
+            set -l ssh_pwd_dir "${homeDirectory}/.ssh/hosts/$host"
+            mkdir -p "$ssh_pwd_dir"
+            printf '\e]7;%s\a' "file://localhost$ssh_pwd_dir" >> /dev/tty
+          '';
+        };
+      };
+      interactiveShellInit = ''
+        function __check_ssh_pwd_dir --on-variable PWD
+          set -l ssh_pwd_pre "${homeDirectory}/.ssh/hosts"
+          if string match -q -- "$ssh_pwd_pre/*" $PWD
+            set -l host (string replace "$ssh_pwd_pre/" "" -- $PWD | cut -d/ -f1)
+            echo -n "SSH to $host? [Y/n] "
+            read -l response
+            if test -z "$response" -o "$response" != "n"
+              echo "Connecting to $host..."
+              exec ssh $host
+            else
+              cd ${homeDirectory}
+            end
+          end
+        end
+      ''
     };
   };
 }
