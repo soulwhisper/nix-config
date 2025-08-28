537e66b501e4e98e832049fe6da223ccb2700e48
Hekatos Noir <soulwhisper@outlook.com>
2025-08-28 20:25:54 +0800

    patch(ssh): simplify ssh config

--- homes/_modules/security/ssh/default.nix
+++ homes/_modules/security/ssh/default.nix
@@ -14,28 +14,8 @@
       ];
 
       matchBlocks = {
-        "192.168.*.*" = {
-          host = "192.168.*.*";
-          sendEnv = [
-            "TERM=xterm-256color"
-          ];
-          extraOptions = {
-            StrictHostKeyChecking = "no";
-            UserKnownHostsFile = "/dev/null";
-          };
-        };
-        "172.16.*.*" = {
-          host = "172.16.*.*";
-          sendEnv = [
-            "TERM=xterm-256color"
-          ];
-          extraOptions = {
-            StrictHostKeyChecking = "no";
-            UserKnownHostsFile = "/dev/null";
-          };
-        };
-        "10.*.*.*" = {
-          host = "10.*.*.*";
+        "internal" = {
+          host = "192.168.*.* 172.16.*.* 10.*.*.* *.homelab.internal";
           sendEnv = [
             "TERM=xterm-256color"
           ];
