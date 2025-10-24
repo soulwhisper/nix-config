b9e37dfac66314f5f075ed76d9465005c4341254
Hekatos Noir <soulwhisper@outlook.com>
2025-10-24 20:34:19 +0800

    patch(svc): fix condition leaking

--- hosts/_modules/nixos/desktop/kde/default.nix
+++ hosts/_modules/nixos/desktop/kde/default.nix
@@ -6,7 +6,7 @@
 }: let
   cfg = config.modules.desktop;
 in {
-  config = lib.mkIf (cfg.manager == "kde") {
+  config = lib.mkIf (cfg.enable && (cfg.manager == "kde")) {
     # KDE Plasma 6, ref:https://wiki.nixos.org/wiki/KDE
     services.xserver.enable = true;
     services.displayManager.sddm = {
