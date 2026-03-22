ebb50ed502e52d66c331ac7d7b7bf0d21b9b2f69
Hekatos Noir <soulwhisper@outlook.com>
2026-03-22 17:09:04 +0800

    patch(host): change address

--- hosts/nix-infra/networking.nix
+++ hosts/nix-infra/networking.nix
@@ -6,17 +6,17 @@
   config = {
     networking = {
       hostName = hostname;
-      nameservers = lib.mkForce ["10.0.0.254"];
+      nameservers = lib.mkForce ["10.10.0.254"];
     };
 
     systemd.network.networks."10-lan" = {
       matchConfig.Name = "ens34"; # proxmox is ens18
       address = [
-        "10.0.0.200/24"
+        "10.10.0.200/24"
       ];
       routes = [
         {
-          Gateway = "10.0.0.1";
+          Gateway = "10.10.0.1";
           GatewayOnLink = true;
         }
       ];
