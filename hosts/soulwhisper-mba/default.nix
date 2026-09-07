5b9e6f0f92e118c27e0b37d9a5381466e9eee026
Hekatos Noir <soulwhisper@outlook.com>
2026-09-07 21:08:58 +0800

    Update default.nix

--- hosts/soulwhisper-mba/default.nix
+++ hosts/soulwhisper-mba/default.nix
@@ -85,6 +85,7 @@
         # :: utilities
         # "nikitabobko/tap/aerospace" # tilling, cant split
         # "jordanbaird-ice" # bartender replacement; check:https://github.com/jordanbaird/Ice/releases/download/0.11.13-dev.2/Ice.zip
+        "vanilla" # ice replacement
         # "karabiner-elements" # not-used
         # "keyboard-maestro" # not-used
         "pixpin" # cleanshotx replacement
