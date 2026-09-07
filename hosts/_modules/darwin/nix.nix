c601870a568d86abc40be3fed4255a03839b592c
Hekatos Noir <soulwhisper@outlook.com>
2026-09-07 20:49:54 +0800

    Update nix.nix

--- hosts/_modules/darwin/nix.nix
+++ hosts/_modules/darwin/nix.nix
@@ -6,11 +6,4 @@ _: {
       Minute = 0;
     };
   };
-
-  # Route daemon fetches through the local proxy; clash TUN fake-ip breaks TLS.
-  # Persists what bootstrap/darwin_set_proxy.py does manually.
-  launchd.daemons.nix-daemon.serviceConfig.EnvironmentVariables = {
-    http_proxy = "http://127.0.0.1:1080";
-    https_proxy = "http://127.0.0.1:1080";
-  };
 }
