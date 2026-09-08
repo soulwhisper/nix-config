64b19c5724cf759b9087323add989f13e85c1ebf
Hekatos Noir <soulwhisper@outlook.com>
2026-09-08 09:34:48 +0800

    chore(mac): default homebrew config cleanup

--- hosts/_modules/darwin/homebrew.nix
+++ hosts/_modules/darwin/homebrew.nix
@@ -1,6 +1,3 @@
-# Core homebrew wiring shared by every darwin host. Desktop apps live in
-# hosts/soulwhisper-mba; headless hosts (soulwhisper-studio) declare only
-# what they serve with.
 _: {
   homebrew = {
     enable = true;
@@ -8,17 +5,6 @@ _: {
       autoUpdate = false; # Don't update during rebuild
       cleanup = "zap"; # Uninstall all programs not declared
       upgrade = true;
-    };
-
-    # : stable apps list
-    # * fix damaged error: `/usr/bin/xattr -cr /Applications/appname.app`
-    taps = [
-    ];
-    brews = [
-    ];
-    casks = [
-    ];
-    masApps = {
     };
   };
 }
